6634d90444f641967549aa7111621d33d29e7c83
Usuario <Usuario@CPD03-M05>
2024-07-29 15:56:32 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppShowDoMilhao/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MauiAppShowDoMilhao.Properties.Models;
+
+namespace MauiAppShowDoMilhao
+{
+
+    List<Pergunta> perguntas_faceis = new()
+    {
+        new Pergunta {
+            Id = 1,
+            Enunciado = "Qual é o nome dado ao estado da água"} 
+    }
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
